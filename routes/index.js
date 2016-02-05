22e6f198977488db1e2b76c6f513857f6e60729d
Kandarp S. Khandwala <kkhandwala@ucsd.edu>
2016-02-05 18:53:59 +0000

    few tweaks for a more complete demo

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,5 @@ var data = require('../data.json');
 
 exports.view = function(req, res){
 	console.log("imagine the (fake) database being spewed here :P") // console.log(data);
-	res.render('index', data);
-	// correct?
+	res.render('index', data); // correct?
 };
