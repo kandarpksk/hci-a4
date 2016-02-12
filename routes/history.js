50807ca92061400cb4b41f9e8250f5dac2928f26
Kandarp S. Khandwala <kkhandwala@ucsd.edu>
2016-02-12 07:43:53 +0000

    before applying finishing touches for the demo

--- routes/history.js
+++ routes/history.js
@@ -1,6 +1,6 @@
 var data = require("../data.json");
 var meals = require('../meals.json');
-var nutrition = require('../nutrition.json');
+// var nutrition = require('../nutrition.json');
 
 exports.view = function(req, res) {
 	res.render('history', meals);
