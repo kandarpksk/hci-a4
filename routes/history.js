ec5bbfafed91c05e4a47b83cd8e8b1037bf30476
Kandarp S. Khandwala <kkhandwala@ucsd.edu>
2016-02-17 23:36:48 +0000

    minor: fixed empty input to previous mail

--- routes/history.js
+++ routes/history.js
@@ -3,7 +3,7 @@ var nutrition = require('../nutrition.json'); // still unused
 
 exports.view = function(req, res) {
 	// add a new meal if at least one food is entered
-	if (req.query.food1 != null) {
+	if (req.query.food1 != null && req.query.food1 != "") {
 		var default_pic = 'http://thumbs.dreamstime.com/m/refresh-ccw-flat-black-color-rounded-vector-icon-symbol-drawn-light-gray-background-57876713.jpg';
 		var newMeal = JSON.parse('{'+
 			'"food1": "' + req.query.food1 + '",'+
