bf1b17ac4528a2c5e783f039e93cabb442868e5b
Kandarp S. Khandwala <kkhandwala@ucsd.edu>
2016-02-25 23:40:43 +0000

    removed old pic. overlay

--- public/js/nutrition-check.js
+++ public/js/nutrition-check.js
@@ -1,9 +1,7 @@
 
-// $(document).ready(function() {
-// })
-
 function info(where) {
 	console.log("called for nutritional information from: "+where)
+	
 	var f = document.forms["addMealForm"]["food1"].value;
 	if(f=="" || f==null)
 		console.log("probably a bug: called for nutritional information with no food");
@@ -45,20 +43,6 @@ function showInfo(result) {
 		<div class="btn-group" role="group" aria-label="...">\
 			<button type="button" class="btn btn-sm btn-default" onclick="pic()">Take a Pic</button>\
 			<button type="button" class="btn btn-sm" onclick="info(\"update button press\")">Nutrition Info.</button>\
-		</div>\
-	</div>';
-}
-
-function pic() {
-	document.getElementById("overlay").innerHTML='\
-	<img id="snap" class="img-responsive img-rounded"\
-	src="https://d13yacurqjgara.cloudfront.net/users/1253/screenshots/1542208/flat-camera-icon.jpg" alt="..."\
-	data-toggle="modal" data-target="#soon">\
-	\
-	<div class="caption">\
-		<div class="btn-group" role="group" aria-label="...">\
-			<button type="button" class="btn btn-sm" onclick="#">Take a Pic</button>\
-			<button type="button" class="btn btn-sm btn-default" onclick="info()">Nutrition Info.</button>\
 		</div>\
 	</div>';
 }
