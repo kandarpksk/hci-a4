c375451df3411f93f19299ad8111676fd5ab982a
Andrew Jabasa <amjabasa@gmail.com>
2016-02-25 22:46:42 -0800

    updates suggestions for each restaurant

--- routes/suggest.js
+++ routes/suggest.js
@@ -1,4 +1,9 @@
+var data = require('../data.json');
 
 exports.view = function(req, res) { 
-	res.render('suggest');
+	console.log("hi");
+	if(req.query.Restaurants == "Panda Express")
+		data[panda] = true;
+	console.log("req.query.Restaurants: " + req.query.Restaurants);
+	res.render('suggest', data);
  }
