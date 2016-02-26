9ed6337104ee7163277f9750c37c5bc6d0bda78f
Kandarp S. Khandwala <kkhandwala@ucsd.edu>
2016-02-26 07:16:41 +0000

    multiple foods' nutritional information partially works

--- public/js/nutrition-check.js
+++ public/js/nutrition-check.js
@@ -1,4 +1,7 @@
 
+var meal_total = [0, 0, 0, 0];
+var reqs = [2000, 65, 50, 25];
+
 function info(where) {
 	console.log("called for nutritional information from: "+where)
 	
@@ -10,8 +13,23 @@ function info(where) {
 	// when there is no food input yet...
 }
 
+var flag = false;
+function reset() {
+	meal_total = [0, 0, 0, 0];
+	flag = true;
+	showInfo(flag);
+}
+
 function showInfo(result) {
-	if((result["total_fat"]*5)/3 > 60)
+	if(!flag) {
+		meal_total[0] += Number(result["calories"]);
+		meal_total[1] += Number(result["total_fat"]);
+		meal_total[2] += Number(result["protein"]);
+		meal_total[3] += Number(result["dietary_fiber"]);
+	}
+	flag = false;
+
+	if((meal_total[1]*100)/reqs[1] > 45)
 		$("#add-meal-button").attr("data-target","#popup-warn");
 	else
 		$("#add-meal-button").attr("data-target","#popup-conf");
@@ -20,26 +38,27 @@ function showInfo(result) {
 	<div id="overlay" style="margin-bottom:-10px;">\
 		<!-- <img alt="pic" src="https://cdn4.iconfinder.com/data/icons/food-icons/128/hamburger.png" style="width:35pt; margin-right:5pt; margin-bottom:10px"> -->\
 		\
-		<h4 align="left">Nutrition Information</h4>\
+		<h4 align="left" style="display:inline;">Nutrition Information</h4>\
+		<button type="button" onClick="reset()">reset</button>\
 		\
 		<div class="progress"> calories\
 			<div class="progress-bar progress-bar-success" style="width: 0%"></div>\
-			<div class="progress-bar progress-bar-warning" style="width: '+result["calories"]/20+'%"></div>\
+			<div class="progress-bar progress-bar-warning" style="width: '+(meal_total[0]*100)/reqs[0]+'%"></div>\
 		</div>\
 		\
-		<div class="progress">\
-			<div class="progress-bar progress-bar-success" style="width: 0%">total fat</div>\
-			<div class="progress-bar progress-bar-warning" style="width: '+(result["total_fat"]*5)/3+'%"></div>\
+		<div class="progress"> total fat\
+			<div class="progress-bar progress-bar-success" style="width: 0%"></div>\
+			<div class="progress-bar progress-bar-warning" style="width: '+(meal_total[1]*100)/reqs[1]+'%"></div>\
 		</div>\
 		\
 		<div class="progress"> protein\
 			<div class="progress-bar progress-bar-success" style="width: 0%"></div>\
-			<div class="progress-bar progress-bar-warning" style="width: '+(result["protein"]*10)/7+'%"></div>\
+			<div class="progress-bar progress-bar-warning" style="width: '+(meal_total[2]*100)/reqs[2]+'%"></div>\
 		</div>\
 		\
 		<div class="progress"> fiber\
 			<div class="progress-bar progress-bar-success" style="width: 0%"></div>\
-			<div class="progress-bar progress-bar-warning" style="width: '+result["dietary_fiber"]/2+'%"></div>\
+			<div class="progress-bar progress-bar-warning" style="width: '+(meal_total[3]*100)/reqs[3]+'%"></div>\
 		</div>\
 	</div>';
 }
