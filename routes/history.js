2e14ea9cf203607dc67402b96980f47df0a4bf18
Kandarp S. Khandwala <kkhandwala@ucsd.edu>
2016-02-29 00:05:18 +0000

    hello with user's name in history, empty history message, proper default picture code

--- routes/history.js
+++ routes/history.js
@@ -3,24 +3,25 @@ var data = require('../data.json');
 exports.view = function(req, res) {
 	// add a new meal if at least one food is entered
 	if (req.query.food1 != null && req.query.food1 != "") {
-		var default_pic = 'http://thumbs.dreamstime.com/m/refresh-ccw-flat-black-color-rounded-vector-icon-symbol-drawn-light-gray-background-57876713.jpg';
 		var newMeal = JSON.parse('{'+
-			'"food1": "' + req.query.food1 + '",'+
-			'"servings1": "' + req.query.servings1 + '", '+
-			'"date": "' + req.query.date + '", '+
-			'"time": "' + req.query.t + '", '+
-			'"pic": "' + default_pic + '" '+
+				'"food1": "' + req.query.food1 + '",'	+ /**/ '"servings1": "' + req.query.servings1 + '", '+
+				'"date": "' + req.query.date + '", '	+ /**/ '"time": "' + req.query.t + '", '+
+				'"pic": "' + '' + '", '					+ /**/ '"snapshots": "' + '' + '" '+
 			'}');
 		
 		for (i = 0; i < data["users"].length; i++)
-			if (data["users"][i]["selected"] != ""){
+			if (data["users"][i]["selected"]){
 				var count = data["users"][i]["meals"].length;
-				if (count > 0){
+				if (count > 0) {
 					if (JSON.stringify(data["users"][i]["meals"][count-1]) != JSON.stringify(newMeal))
 						data["users"][i]["meals"].push(newMeal);
 					// try more variety (or try again)...
 				}
-				else data["users"][i]["meals"].push(newMeal);
+				else {
+					data["users"][i]["meals"].push(newMeal);
+					if (data["users"][i]["empty"])
+						data["users"][i]["empty"] = false;
+				}
 				break;
 			}
 	}
