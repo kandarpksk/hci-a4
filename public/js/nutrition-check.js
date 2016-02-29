0ef9ece60c94539d87486bcac3427dd31b5e1812
Kandarp S. Khandwala <kkhandwala@ucsd.edu>
2016-02-29 03:30:38 +0000

    minor tweaks and fixes; time - 30 minutes

--- public/js/nutrition-check.js
+++ public/js/nutrition-check.js
@@ -1,48 +1,58 @@
 
 var meal_total = [0, 0, 0, 0];
-var reqs = [2000, 65, 50, 25];
 
 function info(where, l) {
 	// console.log("called for nutritional information from: "+where+" with l="+l)
 	
 	meal_total = [0, 0, 0, 0];
-	// console.log("reset done")
+	var temp = ""; // console.log("reset done")
 	for(i=1; i<=l; i++) {
+		temp += "food"+i+" ";
 		var f = document.forms["addMealForm"]["food"+i].value;
 		if(f!="" && f!=null)
 			// might need something called closures
 			$.get("/data/nutrition/"+f, showInfo);
+		else break; // might work
 	}
+	console.log(temp);
 
 	// when there is no food input yet...
 }
 
-var flag = false;
+var reset_flag = false;
 function reset() {
 	meal_total = [0, 0, 0, 0];
-	flag = true;
+	reset_flag = true;
 	var dummy = "dummy";
 	showInfo(dummy);
 }
 
 function showInfo(result) {
+	var reqs = [2000, 65, 50, 25];
+
 	if (result != "dummy") {
 		// console.log("showInfo() called for: (below)");
 		// console.log(result);
 	}
-	if(!flag) {
+
+	if(reset_flag)
+		reset_flag = false
+	else {
 		meal_total[0] += Number(result["calories"]);
 		meal_total[1] += Number(result["total_fat"]);
 		meal_total[2] += Number(result["protein"]);
 		meal_total[3] += Number(result["dietary_fiber"]);
 	}
-	flag = false;
 
 	if((meal_total[1]*100)/reqs[1] > 45)
 		$("#add-meal-button").attr("data-target","#popup-warn");
 	else
-		$("#add-meal-button").attr("data-target","#popup-conf");
+		$("#add-meal-button").attr("data-target","#popup-confirm");
 
+	console.log(parseInt((meal_total[0]*100)/reqs[0])+"% "+
+			parseInt((meal_total[1]*100)/reqs[1])+"% "+
+			parseInt((meal_total[2]*100)/reqs[2])+"% "+
+			parseInt((meal_total[3]*100)/reqs[3])+"%");
 	document.getElementById("calories").setAttribute("style", "width:"+(meal_total[0]*100)/reqs[0]+"%; min-width:2em;");
 	document.getElementById("calories").innerHTML = parseInt((meal_total[0]*100)/reqs[0])+"%";
 	document.getElementById("total fat").setAttribute("style", "width:"+(meal_total[1]*100)/reqs[1]+"%; min-width:2em;");
