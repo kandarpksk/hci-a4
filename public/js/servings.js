64066df3027d4e7c159dc44311f452f86d980be6
Kandarp S. Khandwala <kkhandwala@ucsd.edu>
2016-03-02 00:34:23 +0000

    alt. plus button, error text and servings explanation

--- public/js/servings.js
+++ public/js/servings.js
@@ -3,7 +3,8 @@ function found() { known = true; }
 function explain() {
 	if(!known){
 		window.alert("A serving is how much food a restaurant serves you in a \"regular\" meal.\n\
-For liquids, 1 serving = a small drink (2 servings being a medium drink and so on).");
+For liquids, 1 serving = a small drink (2 servings being a medium drink and so on).\n\
+You may press the question symbol to see this information again.");
 		known = true;
-	} // change to confirm?
+	}
 }
