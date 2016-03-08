c0e1adbb650d2d905f0784f1b901f8044bca4f53
Kandarp S. Khandwala <kkhandwala@ucsd.edu>
2016-03-08 00:32:16 +0000

    login works; no sign-up yet, suggestions page left

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,19 @@ var data = require('../data.json');
 exports.view = function(req, res){
 	data["alt"] = false;
 
+	if(req.query.guest)
+		data["guest"] = true;
+
+	// simple authentication system
+	// identical usernames go through, but passwords must be different
+	//  (relying on chance since this doesn't really matter for the app)
+	if(req.query.username != null && req.query.username != "")
+		for(i = 0; i < data["users"].length; i++)
+			if(data["users"][i]["username"] == req.query.username) {
+				if(data["users"][i]["hash"] == req.query.hash)
+					{data["users"][i]["selected"] = true; data["guest"] = false;}
+			} else data["users"][i]["selected"] = false; // overwrite logins?
+
 	//check data req.
 	
 	var goto_welcome = true;
